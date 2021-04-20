54a2f027056ff8fe321c034a408f1549fd4ba38a
Shivam Kushwaha <shivamk8368@gmail.com>
2021-04-20 16:43:50 -0400

    edited all the content and corrected css issues

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.portfolioPageView , name = "portfolio"),
+    path('resume/', views.resumeView , name = "resume"),
     path('<int:id>', views.projectPageView , name = "project"),
 ]
--- portfolio/views.py
+++ portfolio/views.py
@@ -1,5 +1,10 @@
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, render, HttpResponse
 from .models import Project
+from io import BytesIO
+from django.http import HttpResponse
+from django.template.loader import get_template
+from django.views import View
+from xhtml2pdf import pisa
 
 # Create your views here.
 
@@ -27,3 +32,19 @@ def projectPageView(request , id):
     }
 
     return render(request, 'webpages/projects/project.html', data)
+
+
+def render_to_pdf(template_src, context_dict={}):
+	template = get_template(template_src)
+	html  = template.render(context_dict)
+	result = BytesIO()
+	pdf = pisa.pisaDocument(BytesIO(html.encode("ISO-8859-1")), result)
+	if not pdf.err:
+		return HttpResponse(result.getvalue(), content_type='application/pdf')
+	return None
+
+
+def resumeView(request):
+
+    pdf = render_to_pdf('resume/resume.html')
+    return HttpResponse(pdf, content_type='application/pdf')
